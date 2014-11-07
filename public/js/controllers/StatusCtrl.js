42dccdcd64fba252c2c2b7ea53981db1401c1dda
Thomas Storey <storey.thomas@gmail.com>
2014-11-06 21:35:05 -0500

    can access and read back statuses from the db

--- public/js/controllers/StatusCtrl.js
+++ public/js/controllers/StatusCtrl.js
@@ -1,5 +1,14 @@
 // public/js/controllers/StatusCtrl.js
 
-angular.module('StatusCtrl', []).controller('StatusController', function($scope) {
+angular.module('StatusCtrl', []).controller('StatusController', function($scope, $http) {
 	$scope.title = 'statuses';
+
+	$scope.statuses =  {};
+	$http.get('/api/statuses')
+		.success(function(data){
+			$scope.statuses = data;
+		})
+		.error(function(data){
+			console.log(data);
+		});
 });
